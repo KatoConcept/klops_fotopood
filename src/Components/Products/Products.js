8593d03c8febc3c4459aed15b7b3fc8173a71c2e
erikKato <markuserik.kato@tptlive.ee>
2020-12-14 18:03:36 +0200

    Product cards ready

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -60,7 +60,7 @@ function Products() {
                 </div>
                 <div className="custom-control custom-radio custom-control-inline checkbox">
                   <input type="checkbox" id="filter-check-btn-8" name="filter-check-btn-8" className="custom-control-input" />
-                  <label className="custom-control-label" for="filter-check-btn-8">Filtrid</label>
+                  <label className="custom-control-label" for="filter-check-btn-8">Muud tarvikud</label>
                 </div>
               </div>
             </div>
@@ -125,15 +125,78 @@ function Products() {
 
         <div className="spacer"></div>
 
-        <div className="products-container view-width">
+        <div className="products-container">
           <div className="products">
-            <a href="/">
               <div className="product-card">
-                <div className="product-card-thumbnail"></div>
-                <h2 className="product-card-title">Nikon Z7</h2>
-                <h1 className="product-card-price">2799.90€</h1>
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-1-thumbnail"></div>
+                  <h2 className="product-card-title">Nikon Z7</h2>
+                  <h1 className="product-card-price">2799.90€</h1>
+                </a>
               </div>
-            </a>
+              <div className="product-card">
+              <a href="/">
+                <div className="product-card-thumbnail" id="product-2-thumbnail"></div>
+                <h2 className="product-card-title">Sigma 35mm f/1.4</h2>
+                <h1 className="product-card-price">749.90€</h1>
+              </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-3-thumbnail"></div>
+                  <h2 className="product-card-title">EN-EL15 Lithium-Ion Battery Pack (7.0V, 1900mAh)</h2>
+                  <h1 className="product-card-price">29.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-4-thumbnail"></div>
+                  <h2 className="product-card-title">Delkin Devices 64GB Premium XQD Mälukaart</h2>
+                  <h1 className="product-card-price">119.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-5-thumbnail"></div>
+                  <h2 className="product-card-title">Nikon Nikkor Z 50mm f/1.8 S</h2>
+                  <h1 className="product-card-price">499.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-6-thumbnail"></div>
+                  <h2 className="product-card-title">Nikon FTZ Lens Adapter f. F-Lens to Z-Kamera</h2>
+                  <h1 className="product-card-price">429.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-7-thumbnail"></div>
+                  <h2 className="product-card-title">Kata Lite-433 DL Fotokott</h2>
+                  <h1 className="product-card-price">39.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-8-thumbnail"></div>
+                  <h2 className="product-card-title">Sony XQD & SD Kaardilugeja</h2>
+                  <h1 className="product-card-price">109.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-9-thumbnail"></div>
+                  <h2 className="product-card-title">Nikon akulaadija MH-25a EN-EL15a</h2>
+                  <h1 className="product-card-price">74.90€</h1>
+                </a>
+              </div>
+              <div className="product-card">
+                <a href="/">
+                  <div className="product-card-thumbnail" id="product-10-thumbnail"></div>
+                  <h2 className="product-card-title">Nikon välklamp SB-700</h2>
+                  <h1 className="product-card-price">329.90€</h1>
+                </a>
+              </div>
           </div>
         </div>
       </section>
