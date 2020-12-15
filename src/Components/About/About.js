b575b5cf0932b0623854926db5c2ac16ce014b19
erikKato <markuserik.kato@tptlive.ee>
2020-12-15 03:58:14 +0200

    Testing burger menu

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,17 +3,9 @@ import { Link } from "react-router-dom";
 function About() {
     document.title = "Klõps | Meie";
   return (
-    <footer class="footer">
-      <div class="footer-container">
-        <Link to="/">
-          <img src="./img/logo2_png.png" height="30px" alt="" />
-        </Link>
-        <p class="footer-text">
-          Klõps OÜ | Pärnu mnt 57a, 10135 Tallinn | info@klops.ee
-        </p>
-        <p class="footer-text">© Klõps OÜ - kõik õigused kaitstud</p>
-      </div>
-    </footer>
+    <div className="about-page">
+
+    </div>
   );
 };
 
