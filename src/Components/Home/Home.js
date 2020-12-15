a0f974fc04a6a66cd34bb44170bc59b5b5b64c0b
erikKato <markuserik.kato@tptlive.ee>
2020-12-15 04:28:30 +0200

    Starting to make landing page responsive

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,30 @@ function Home() {
       <div className="main-container view-width">
         <div className="text-area">
           <h1>Kõik Sinule ja Sinu fotode jaoks vajalik ühest kohast!</h1>
-          <a href="/products" className="btn">Vaata tooteid</a>
+          <div className="slideshow-wrapper">
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel" data-interval="false">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src="../img/product-1.jpg" alt="First slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="../img/product-1-2.jpg" alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="../img/product-1-3.jpg" alt="Third slide" />
+                </div>
+              </div>
+              <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+          <a href="/products" className="btn check-products">Vaata tooteid</a>
         </div>
       </div>
     </section>
