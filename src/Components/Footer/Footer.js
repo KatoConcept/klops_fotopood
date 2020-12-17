35400a8c12a2db231bec5460073ce26d76deb649
erikKato <markuserik.kato@tptlive.ee>
2020-12-17 20:43:38 +0200

    Added back to top button

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,25 @@
 import { Link } from "react-router-dom";
-import React from 'react';
+import React, { useState } from 'react';
 
 function Footer() {
+  const [backToTop, setBackToTop] = useState(false);
+
+  const addBackToTop = () => {
+    if (window.scrollY > 700) {
+      setBackToTop(true)
+    } else {
+      setBackToTop(false)
+    }
+  };
+
+  function GoBackToTop() {
+    window.scroll({ top: 0, behavior: 'smooth' });
+    
+  }
+
+  window.addEventListener('scroll', addBackToTop);
+
+
   return (
     <footer className="footer">
       <div className="footer-padding"></div>
@@ -15,6 +33,7 @@ function Footer() {
         <p className="footer-text">© Klõps OÜ - kõik õigused kaitstud</p>
       </div>
       <div className="footer-padding"></div>
+      <div className={backToTop ? 'back-to-top-btn active' : 'back-to-top-btn'} onClick={GoBackToTop}><i class="fa fa-chevron-up"></i></div>
     </footer>
   );
 };
